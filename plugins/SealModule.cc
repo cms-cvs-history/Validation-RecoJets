c1c1f4ac1c3086574cadef3acbd436358436c3a9
Roger Wolf <sha1-bf6c89fe049b9f9a193bed584ffb80166e4ef256@cern.ch>
2008-06-12 20:13:54 +0000

    added parton correction module

--- plugins/SealModule.cc
+++ plugins/SealModule.cc
@@ -5,11 +5,12 @@
 #include "Validation/RecoJets/plugins/Balancing_fwd.h"
 #include "Validation/RecoJets/plugins/CalibAnalyzer.h"
 
-typedef CalibAnalyzer<reco::GenJetCollection, GenJetMatch> GenJetClosure;
-typedef CalibAnalyzer<reco::CaloJetCollection, CaloJetMatch> CompareCalibs;
-typedef CalibAnalyzer<reco::GenParticleCollection, PartonMatch> PartonClosure;
+typedef CalibAnalyzer<reco::GenJetCollection, GenJetCaloJetMatch> GenJetClosure;
+typedef CalibAnalyzer<reco::CaloJetCollection, CaloJetCaloJetMatch> CompareCalibs;
+typedef CalibAnalyzer<reco::GenParticleCollection, PartonCaloJetMatch> PartonClosure;
+typedef CalibAnalyzer<reco::GenParticleCollection, PartonGenJetMatch> PartonCorrection;
 
-typedef CalibAnalyzer<reco::PhotonCollection, PhotonJetBalance> PhotonJetClosure;
+typedef CalibAnalyzer<reco::PhotonCollection, PhotonCaloJetBalance> PhotonJetClosure;
 
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE( CompareCalibs );
