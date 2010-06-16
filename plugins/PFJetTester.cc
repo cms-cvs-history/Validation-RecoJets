2b3c84431494c8a84e83c56122653b37452e1429
Salvatore Rappoccio <sha1-15b661ca0cbf207b4ae3d8fa8842a4216051970d@cern.ch>
2010-06-16 19:25:43 +0000

    Minimal backport of reco jets migration

--- plugins/PFJetTester.cc
+++ plugins/PFJetTester.cc
@@ -1,7 +1,7 @@
 // Producer for validation histograms for CaloJet objects
 // F. Ratnikov, Sept. 7, 2006
 // Modified by J F Novak July 10, 2008
-// $Id: PFJetTester.cc,v 1.10 2010/02/13 03:06:52 hatake Exp $
+// $Id: PFJetTester.cc,v 1.11 2010/03/26 01:27:54 hatake Exp $
 
 #include "FWCore/MessageLogger/interface/MessageLogger.h"
 #include "FWCore/Framework/interface/Event.h"
@@ -26,7 +26,7 @@
 #include "DataFormats/METReco/interface/MET.h"
 #include "DataFormats/METReco/interface/METCollection.h"
 
-#include "RecoJets/JetAlgorithms/interface/JetMatchingTools.h"
+#include "RecoJets/JetProducers/interface/JetMatchingTools.h"
 
 #include "SimDataFormats/GeneratorProducts/interface/HepMCProduct.h"
 
